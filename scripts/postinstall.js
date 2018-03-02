30710c005dc5837d55e32f7f7106cfc1adf785d3
rdfriedl <rdfriedl@gmail.com>
2018-03-01 18:04:13 -0600

    remove postinstall script
    add fontawesome icon packs to peerDependencies
    remove package-lock.json

--- scripts/postinstall.js
+++
@@ -1,3 +0,0 @@
-var shell = require('shelljs');
-
-shell.exec("yarn add @fortawesome/fontawesome-pro-light @fortawesome/fontawesome-pro-regular @fortawesome/fontawesome-pro-solid --ignore-scripts");
