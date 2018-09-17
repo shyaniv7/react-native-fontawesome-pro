a1615933bcc8e9fa85475249da4784075877f3cb
Michiel van Roon <michiel@michielvanroon.nl>
2018-09-17 16:37:54 +0200

    added typings

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+import { ViewStyle, ViewProperties } from "react-native";
+import { SvgProps } from "react-native-svg";
+
+export interface IconProps extends ViewProperties {
+  name: string;
+  size: number;
+  color: string;
+  type: "regular" | "light" | "solid" | "brands";
+  containerStyle: ViewStyle;
+  iconStyle: ViewStyle;
+  onPress?: () => void;
+  activeOpacity: number;
+}
+
+export function configureFontAwesomePro(): void;
+
+declare const Icon: React.Component<IconProps>;
+export default Icon;
