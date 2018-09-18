e08cab3e0a399fb9273ff684a147d00cb9b4d7f4
Michiel van Roon <michiel@michielvanroon.nl>
2018-09-18 10:44:17 +0200

    made props optional and fixed export class

--- index.d.ts
+++ index.d.ts
@@ -2,17 +2,17 @@ import { ViewStyle, ViewProperties } from "react-native";
 import { SvgProps } from "react-native-svg";
 
 export interface IconProps extends ViewProperties {
-  name: string;
-  size: number;
-  color: string;
-  type: "regular" | "light" | "solid" | "brands";
-  containerStyle: ViewStyle;
-  iconStyle: ViewStyle;
+  name?: string;
+  size?: number;
+  color?: string;
+  type?: "regular" | "light" | "solid" | "brands";
+  containerStyle?: ViewStyle;
+  iconStyle?: ViewStyle;
   onPress?: () => void;
-  activeOpacity: number;
+  activeOpacity?: number;
 }
 
 export function configureFontAwesomePro(): void;
 
-declare const Icon: React.Component<IconProps>;
+declare const Icon: React.ComponentClass<IconProps>;
 export default Icon;
